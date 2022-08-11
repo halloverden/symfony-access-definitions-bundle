21df4fe87a12a51cec5884961f13edfd599bc8b1
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-11 15:05:15 +0200

    FIX: namespace

--- src/HalloVerdenSymfonyAccessDefinitionsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HalloVerden\SymfonyAccessDefinitionsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class HalloVerdenSymfonyAccessDefinitionsBundle extends Bundle {
-
-}
